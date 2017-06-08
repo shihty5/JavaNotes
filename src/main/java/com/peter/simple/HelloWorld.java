43c4d09f2b8312700877e3a6ef9e98e3db252461
Peter <ts67954@imcap.ap.ssmb.com>
2017-06-08 11:01:51 +0800

    first commit

---
+++ src/main/java/com/peter/simple/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.peter.simple;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ src/test/java/com/peter/simple/HellWorldTest.java
@@ -0,0 +1,5 @@
+package com.peter.simple;
+
+public class HellWorldTest {
+
+}
