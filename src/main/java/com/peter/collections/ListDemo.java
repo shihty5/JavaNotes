a094b334865dd9cff31b8c02704890e4250f88c1
Peter <shihty5@163.com>
2017-06-25 17:11:41 +0800

    add Maven dependency and Jedis jar

---
+++ src/main/java/com/peter/collections/ListDemo.java
@@ -0,0 +1,14 @@
+package com.peter.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(5);
+		System.out.println(list);
+	}
+
+}
